a04d498b8393a2dd86fdfbe4e5dfac9eb68593a5
Pedro Henrique Sanson <156597965+sansonpedro@users.noreply.github.com>
2025-10-01 16:06:22 -0300

    integrando fontes e estilização

--- backend/src/database/conexao.js
+++ backend/src/database/conexao.js
@@ -1,27 +1,44 @@
+// import mysql2 from "mysql2/promise";
+
+// async function conectarBd() {
+//   if (global.conexao) return global.conexao;
+
+//   try {
+//     const conexao = await mysql2.createConnection({
+//       host: "localhost",
+//       database: "novoBd",
+//       password: "",
+//       user: "root",
+//       port: 3306,
+//     });
+//     global.conexao = conexao;
+//     return conexao;
+//   } catch (e) {
+//     console.log(e);
+//   }
+// }
+
+// export default conectarBd;
+
 import mysql2 from "mysql2/promise";
 
 async function conectarBd() {
+  if (global.conexao) return global.conexao;
 
-    if (global.conexao)
-        return global.conexao;
-
-    try {
-
-        const conexao = await mysql2.createConnection({
-            host: 'localhost',
-            database: 'novoBd',
-            password: 'root',
-            user: 'root',
-            port: 3307
-        });
-        global.conexao = conexao;
-        return conexao;
-
-    }
-    catch (e) {
-        console.log(e);
-    }
-
+  try {
+    const conexao = await mysql2.createConnection({
+      host: "localhost",
+      database: "novoBd",
+      password: "root",
+      user: "root",
+      port: 3307,
+    });
+    global.conexao = conexao;
+    return conexao;
+  } catch (e) {
+    console.log(e);
+  }
 }
 
 export default conectarBd;
+
