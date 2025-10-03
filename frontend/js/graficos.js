c8216ee8f26e7335cee4e68592dac13092104179
Pedro Henrique Sanson <156597965+sansonpedro@users.noreply.github.com>
2025-10-03 16:10:34 -0300

    estilizando

--- frontend/js/graficos.js
+++ frontend/js/graficos.js
@@ -1,210 +1,224 @@
 import {
-    getQuantidadeMetrosProduzidoPorTarefaNoMes,
-    getQuantidadeTempoDeProducaoPorDia,
-    formater,
-    anoAtual,
-    proximoMesAtual,
-    anteriorMesAtual,
-    mesAtual
+  anoAtual,
+  anteriorMesAtual,
+  getQuantidadeMetrosProduzidoPorTarefaNoMes,
+  getQuantidadeTempoDeProducaoPorDia,
+  mesAtual,
+  proximoMesAtual,
 } from "./helpers.js";
 
 import {
-    aumentarAno,
-    diminuirAno,
-    operandoMes,
-    operacaoDiminuirMes,
-    operacaoAumentarMes
+  aumentarAno,
+  diminuirAno,
+  operacaoAumentarMes,
+  operacaoDiminuirMes,
+  operandoMes,
 } from "./helpers/funcoes_alterar_mes_ano.js";
 
-
 window.onload = function () {
+  let grafico1;
+  let grafico2;
+  let grafico3;
 
-    let grafico1;
-    let grafico2;
-    let grafico3;
+  let dadosPrimeiroGraficoLinha;
+  let dadosPrimeiroGraficoBarra;
 
-    let dadosPrimeiroGraficoLinha;
-    let dadosPrimeiroGraficoBarra;
+  let mesSelecionadoUser = mesAtual;
+  let mesAnteriorUser = anteriorMesAtual;
+  let mesProximoUser = proximoMesAtual;
+  let anoAtualUser = anoAtual;
 
-    let mesSelecionadoUser = mesAtual;
-    let mesAnteriorUser = anteriorMesAtual;
-    let mesProximoUser = proximoMesAtual;
-    let anoAtualUser = anoAtual;
+  const btnDiminuirMes = $("#btnDiminuirMes");
+  const btnAumentarMes = $("#btnAumentarMes");
+  const btnDiminuirAno = $("#btnDiminuirAno");
+  const btnAumentarAno = $("#btnAumentarAno");
 
-    const btnDiminuirMes = $('#btnDiminuirMes');
-    const btnAumentarMes = $('#btnAumentarMes');
-    const btnDiminuirAno = $('#btnDiminuirAno');
-    const btnAumentarAno = $('#btnAumentarAno');
+  const anoSelecionado = $("#anoSelecionado");
 
-    const anoSelecionado = $('#anoSelecionado');
+  btnAumentarAno.click(async function () {
+    anoAtualUser = aumentarAno(anoAtualUser, anoSelecionado);
 
-    btnAumentarAno.click(async function () {
-        anoAtualUser = aumentarAno(anoAtualUser, anoSelecionado);
+    dadosPrimeiroGraficoLinha = await getQuantidadeTempoDeProducaoPorDia(
+      anoAtualUser,
+      (mesSelecionadoUser + 1).toString().padStart(2, 0)
+    );
+    dadosPrimeiroGraficoBarra = await getQuantidadeMetrosProduzidoPorTarefaNoMes(
+      anoAtualUser,
+      (mesSelecionadoUser + 1).toString().padStart(2, 0)
+    );
 
-        dadosPrimeiroGraficoLinha = await getQuantidadeTempoDeProducaoPorDia(anoAtualUser, (mesSelecionadoUser + 1).toString().padStart(2, 0));
-        dadosPrimeiroGraficoBarra = await getQuantidadeMetrosProduzidoPorTarefaNoMes(anoAtualUser, (mesSelecionadoUser + 1).toString().padStart(2, 0));
+    construirGraficoPorMetrosProduzidosPorNumTarefa();
+    construirGraficoPorTempoProduzidoPorNumTarefa();
+    construirGraficoLinhaPorTempoProduzido();
+  });
 
-        construirGraficoPorMetrosProduzidosPorNumTarefa();
-        construirGraficoPorTempoProduzidoPorNumTarefa();
-        construirGraficoLinhaPorTempoProduzido();
+  btnDiminuirAno.click(async function () {
+    anoAtualUser = diminuirAno(anoAtualUser, anoSelecionado);
+
+    dadosPrimeiroGraficoLinha = await getQuantidadeTempoDeProducaoPorDia(
+      anoAtualUser,
+      (mesSelecionadoUser + 1).toString().padStart(2, 0)
+    );
+    dadosPrimeiroGraficoBarra = await getQuantidadeMetrosProduzidoPorTarefaNoMes(
+      anoAtualUser,
+      (mesSelecionadoUser + 1).toString().padStart(2, 0)
+    );
+
+    construirGraficoPorMetrosProduzidosPorNumTarefa();
+    construirGraficoPorTempoProduzidoPorNumTarefa();
+    construirGraficoLinhaPorTempoProduzido();
+  });
+
+  const mesSelecionado = $("#mesSelecionado");
+
+  async function mudarMesSelecionado(operacao) {
+    let meses = operandoMes(mesAnteriorUser, mesSelecionadoUser, mesProximoUser, mesSelecionado, operacao);
+
+    mesAnteriorUser = meses.mesAnteriorUser;
+    mesSelecionadoUser = meses.mesSelecionadoUser;
+    mesProximoUser = meses.mesProximoUser;
+
+    dadosPrimeiroGraficoLinha = await getQuantidadeTempoDeProducaoPorDia(
+      anoAtualUser,
+      (mesSelecionadoUser + 1).toString().padStart(2, 0)
+    );
+    dadosPrimeiroGraficoBarra = await getQuantidadeMetrosProduzidoPorTarefaNoMes(
+      anoAtualUser,
+      (mesSelecionadoUser + 1).toString().padStart(2, 0)
+    );
+
+    console.log(dadosPrimeiroGraficoBarra);
+
+    construirGraficoPorMetrosProduzidosPorNumTarefa();
+    construirGraficoPorTempoProduzidoPorNumTarefa();
+    construirGraficoLinhaPorTempoProduzido();
+  }
+
+  btnDiminuirMes.click(async function () {
+    let meses = operacaoDiminuirMes({ mesAnteriorUser, mesSelecionadoUser, mesProximoUser });
+
+    mesAnteriorUser = meses.mesAnteriorUser;
+    mesSelecionadoUser = meses.mesSelecionadoUser;
+    mesProximoUser = meses.mesProximoUser;
+
+    mudarMesSelecionado("-");
+  });
+
+  btnAumentarMes.click(async function () {
+    let meses = operacaoAumentarMes({ mesAnteriorUser, mesSelecionadoUser, mesProximoUser });
+
+    mesAnteriorUser = meses.mesAnteriorUser;
+    mesSelecionadoUser = meses.mesSelecionadoUser;
+    mesProximoUser = meses.mesProximoUser;
+
+    mudarMesSelecionado("+");
+  });
+
+  const graficoNumMetrosTarefa = document.getElementById("graficoLinhaTotalTempoTarefa");
+  const graficoBarTempoProduzido = document.getElementById("graficoBarTempoProduzido");
+  const graficoLinhaTotalTempoTarefa = document.getElementById("graficoLinhaTempoProduzidoPorTarefa");
+
+  function construirGraficoPorMetrosProduzidosPorNumTarefa() {
+    if (grafico1) grafico1.destroy();
+    grafico1 = new Chart(graficoBarTempoProduzido, {
+      type: "bar",
+      data: {
+        labels: dadosPrimeiroGraficoBarra.map((dados) => dados.data_historico),
+        datasets: [
+          {
+            label: "Quantidade de tempo de produção",
+            data: dadosPrimeiroGraficoBarra.map((dados) => dados.tempo_producao),
+            borderWidth: 1,
+          },
+        ],
+      },
+      options: {
+        scales: {
+          y: {
+            beginAtZero: true,
+          },
+        },
+        plugins: {
+          display: true,
+        },
+      },
     });
+  }
 
-
-    btnDiminuirAno.click(async function () {
-        anoAtualUser = diminuirAno(anoAtualUser, anoSelecionado);
-
-        dadosPrimeiroGraficoLinha = await getQuantidadeTempoDeProducaoPorDia(anoAtualUser, (mesSelecionadoUser + 1).toString().padStart(2, 0));
-        dadosPrimeiroGraficoBarra = await getQuantidadeMetrosProduzidoPorTarefaNoMes(anoAtualUser, (mesSelecionadoUser + 1).toString().padStart(2, 0));
-
-        construirGraficoPorMetrosProduzidosPorNumTarefa();
-        construirGraficoPorTempoProduzidoPorNumTarefa();
-        construirGraficoLinhaPorTempoProduzido();
-
-    })
-
-    const mesSelecionado = $('#mesSelecionado');
-
-    async function mudarMesSelecionado(operacao) {
-        let meses = operandoMes(mesAnteriorUser, mesSelecionadoUser, mesProximoUser, mesSelecionado, operacao);
-
-        mesAnteriorUser = meses.mesAnteriorUser;
-        mesSelecionadoUser = meses.mesSelecionadoUser;
-        mesProximoUser = meses.mesProximoUser;
-
-        dadosPrimeiroGraficoLinha = await getQuantidadeTempoDeProducaoPorDia(anoAtualUser, (mesSelecionadoUser + 1).toString().padStart(2, 0));
-        dadosPrimeiroGraficoBarra = await getQuantidadeMetrosProduzidoPorTarefaNoMes(anoAtualUser, (mesSelecionadoUser + 1).toString().padStart(2, 0));
-
-        console.log(dadosPrimeiroGraficoBarra);
-
-
-        construirGraficoPorMetrosProduzidosPorNumTarefa();
-        construirGraficoPorTempoProduzidoPorNumTarefa();
-        construirGraficoLinhaPorTempoProduzido();
-    }
-
-    btnDiminuirMes.click(async function () {
-        let meses = operacaoDiminuirMes({ mesAnteriorUser, mesSelecionadoUser, mesProximoUser })
-
-        mesAnteriorUser = meses.mesAnteriorUser;
-        mesSelecionadoUser = meses.mesSelecionadoUser;
-        mesProximoUser = meses.mesProximoUser;
-
-        mudarMesSelecionado('-');
+  function construirGraficoPorTempoProduzidoPorNumTarefa() {
+    if (grafico2) grafico2.destroy();
+    grafico2 = new Chart(graficoNumMetrosTarefa, {
+      type: "line",
+      data: {
+        labels: dadosPrimeiroGraficoLinha.map((dados) => dados.numero_tarefa),
+        datasets: [
+          {
+            label: "Total produzido por tarefa",
+            data: dadosPrimeiroGraficoLinha.map((dados) => dados.total_metros_da_tarefa),
+            borderWidth: 1,
+          },
+        ],
+      },
+      options: {
+        plugins: {
+          legend: {
+            display: true,
+            position: "top",
+            labels: {
+              textAlign: "center",
+              fontSize: 10,
+            },
+          },
+        },
+      },
     });
+  }
 
-    btnAumentarMes.click(async function () {
-        let meses = operacaoAumentarMes({ mesAnteriorUser, mesSelecionadoUser, mesProximoUser })
+  function construirGraficoLinhaPorTempoProduzido() {
+    if (grafico3) grafico3.destroy();
 
-        mesAnteriorUser = meses.mesAnteriorUser;
-        mesSelecionadoUser = meses.mesSelecionadoUser;
-        mesProximoUser = meses.mesProximoUser;
-
-        mudarMesSelecionado('+');
+    grafico3 = new Chart(graficoLinhaTotalTempoTarefa, {
+      type: "line",
+      data: {
+        labels: dadosPrimeiroGraficoLinha.map((dados) => dados.numero_tarefa),
+        datasets: [
+          {
+            label: "Tempo produzido por tarefa",
+            data: dadosPrimeiroGraficoLinha.map((dados) => dados.tempo_producao),
+            borderWidth: 1,
+          },
+        ],
+      },
+      options: {
+        plugins: {
+          legend: {
+            display: true,
+            position: "top",
+            labels: {
+              textAlign: "center",
+              fontSize: 10,
+            },
+          },
+        },
+      },
     });
+  }
 
-    const graficoNumMetrosTarefa = document.getElementById('graficoLinhaTotalTempoTarefa');
-    const graficoBarTempoProduzido = document.getElementById('graficoBarTempoProduzido');
-    const graficoLinhaTotalTempoTarefa = document.getElementById('graficoLinhaTempoProduzidoPorTarefa');
+  (async () => {
+    dadosPrimeiroGraficoBarra = await getQuantidadeMetrosProduzidoPorTarefaNoMes(
+      anoAtualUser,
+      (mesSelecionadoUser + 1).toString().padStart(2, 0)
+    );
+    dadosPrimeiroGraficoLinha = await getQuantidadeTempoDeProducaoPorDia(
+      anoAtualUser,
+      (mesSelecionadoUser + 1).toString().padStart(2, 0)
+    );
 
-    function construirGraficoPorMetrosProduzidosPorNumTarefa() {
+    construirGraficoPorMetrosProduzidosPorNumTarefa();
+    construirGraficoPorTempoProduzidoPorNumTarefa();
+    construirGraficoLinhaPorTempoProduzido();
 
-        if (grafico1) grafico1.destroy();
-        grafico1 = new Chart(graficoBarTempoProduzido, {
-            type: 'bar',
-            data: {
-                labels: dadosPrimeiroGraficoBarra.map((dados) => dados.data_historico),
-                datasets: [{
-                    label: "Quantidade de tempo de produção",
-                    data: dadosPrimeiroGraficoBarra.map((dados) => dados.tempo_producao),
-                    borderWidth: 1
-                }]
-            },
-            options: {
-                scales: {
-                    y: {
-                        beginAtZero: true
-                    }
-                },
-                plugins: {
-                    display: true
-                }
-            }
-        });
-
-    }
-
-    function construirGraficoPorTempoProduzidoPorNumTarefa() {
-
-        if (grafico2) grafico2.destroy();
-        grafico2 = new Chart(graficoNumMetrosTarefa, {
-            type: 'line',
-            data: {
-                labels: dadosPrimeiroGraficoLinha.map((dados) => dados.numero_tarefa),
-                datasets: [
-                    {
-                        label: "Total produzido por tarefa",
-                        data: dadosPrimeiroGraficoLinha.map((dados) => dados.total_metros_da_tarefa),
-                        borderWidth: 1
-                    }
-                ]
-            },
-            options: {
-                plugins: {
-                    legend: {
-                        display: true,
-                        position: 'top',
-                        labels: {
-                            textAlign: 'center',
-                            fontSize: 10
-                        }
-                    }
-                }
-            }
-        });
-
-    }
-
-    function construirGraficoLinhaPorTempoProduzido() {
-
-        if (grafico3) grafico3.destroy();
-
-        grafico3 = new Chart(graficoLinhaTotalTempoTarefa, {
-            type: 'line',
-            data: {
-                labels: dadosPrimeiroGraficoLinha.map((dados) => dados.numero_tarefa),
-                datasets: [
-                    {
-                        label: "Tempo produzido por tarefa",
-                        data: dadosPrimeiroGraficoLinha.map((dados) => dados.tempo_producao),
-                        borderWidth: 1
-                    }
-                ]
-            },
-            options: {
-                plugins: {
-                    legend: {
-                        display: true,
-                        position: 'top',
-                        labels: {
-                            textAlign: 'center',
-                            fontSize: 10
-                        }
-                    }
-                }
-            }
-        });
-    }
-
-    (async () => {
-        dadosPrimeiroGraficoBarra = await getQuantidadeMetrosProduzidoPorTarefaNoMes(anoAtualUser, (mesSelecionadoUser + 1).toString().padStart(2, 0));
-        dadosPrimeiroGraficoLinha = await getQuantidadeTempoDeProducaoPorDia(anoAtualUser, (mesSelecionadoUser + 1).toString().padStart(2, 0));
-
-        construirGraficoPorMetrosProduzidosPorNumTarefa();
-        construirGraficoPorTempoProduzidoPorNumTarefa();
-        construirGraficoLinhaPorTempoProduzido();
-
-        mudarMesSelecionado();
-        anoSelecionado.text(anoAtual);
-    })()
-
-}
+    mudarMesSelecionado();
+    anoSelecionado.text(anoAtual);
+  })();
+};
