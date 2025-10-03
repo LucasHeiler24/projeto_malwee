1c1def9a125df1c02272ad8121caab0277f05dca
Pedro Henrique Sanson <156597965+sansonpedro@users.noreply.github.com>
2025-10-03 16:40:53 -0300

    porta correta

--- backend/src/database/EntidadeDados.js
+++ backend/src/database/EntidadeDados.js
@@ -4,10 +4,9 @@ async function pegarDadosMesEAnoEscolhido(mesEscolhidoEAno) {
   try {
     const conexao = await conectarBd();
 
-    // const [rows] = await conexao.query("SELECT * FROM dados WHERE data_historico LIKE ?", [`%${mesEscolhidoEAno}%`]);
+    const [rows] = await conexao.query("SELECT * FROM dados WHERE data_historico LIKE ?", [`%${mesEscolhidoEAno}%`]);
 
-    const [rows] = await conexao.query("SELECT * FROM novocsv WHERE data_historico LIKE ?", [`%${mesEscolhidoEAno}%`]);
-    
+    // const [rows] = await conexao.query("SELECT * FROM novocsv WHERE data_historico LIKE ?", [`%${mesEscolhidoEAno}%`]);
 
     return rows;
   } catch (e) {
