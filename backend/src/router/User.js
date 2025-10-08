f9d3b5e0be11268df2891839457ad0004cda06a9
LUCAS HEILER <lucas_heiler@estudante.sesisenai.org.br>
2025-10-08 15:52:28 -0300

    Backend atualizado

--- backend/src/router/User.js
+++ backend/src/router/User.js
@@ -1,9 +1,9 @@
 import { Router } from "express";
 import { create, loginStore } from "../controllers/UserController.js";
-import {validationsUser, validationLogin} from "../controllers/validations/validationUser.js";
+import {validationsUser, validationLogin} from "../validations/validationUser.js";
 
 const routerUser = Router();
 routerUser.post('/create', validationsUser, create);
-routerUser.post('/store', validationLogin, loginStore)
+routerUser.post('/store', validationLogin, loginStore);
 
 export default routerUser;
