62abda71570dd0c2d65fea76bb7234dea8a402e1
andrey1693 <andrey_castilho@estudante.sesisenai.org.br>
2025-10-17 14:19:36 -0300

    Update conexao.js

--- backend/src/database/conexao.js
+++ backend/src/database/conexao.js
@@ -7,10 +7,18 @@ async function conectarBd() {
 
   try {
     const conexao = await mysql2.createConnection({
+<<<<<<< Updated upstream
       host: process.env.HOST_DATABASE,
       database: process.env.NAME_DATABASE,
       password: process.env.PASSWORD_DATABASE,
       user: process.env.USER_DATABASE
+=======
+      host: "localhost",
+      database: "novoBd",
+      password: "",
+      user: "root",
+      port: 3306,
+>>>>>>> Stashed changes
     });
     global.conexao = conexao;
     return conexao;
